8249ea80ed21979be98c4c5182744ac43663aa67
justinmorales1 <justin@justins-mbp.lan>
2021-02-04 08:32:56 -0600

    Adding react router dependencies and adding to App file

---
+++ src/components/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+
+const PageOne = () => {
+    return <div>Page One</div>
+};
+
+const PageTwo = () => {
+    return (
+        <div>Page Two
+            <button>Button</button>
+         </div>)
+};
+
+const App = () => {
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={PageOne}/>
+                    <Route path="/pagetwo" component={PageTwo}/>
+                </div>
+            </BrowserRouter>
+        </div>
+    )
+};
+
+
+export default App;
