ce2d83733c11aabe9b9b8bedb8fb3af5055df51c
justinmorales1 <justin@justins-mbp.lan>
2021-02-06 15:02:35 -0600

    Added reduxForm and some initial forms to stream create

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
+import { reducer as formReducer } from 'redux-form';
+import  {createSotre, applyMiddleware, comepose} from "redux";
 
 
 export default combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 })
